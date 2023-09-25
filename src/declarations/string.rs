687e1a08b9596fd17e634130964384feef654aac
JonasJore <jonas.jore@gmail.com>
2023-09-25 13:52:15 +0200

    removed unneeded function for gen 9 early days since it is outdated

--- src/declarations/string.rs
+++ src/declarations/string.rs
@@ -1,18 +1,4 @@
-use crate::constants;
-
-fn generation_nine_early_days_warning() {
-    println!(
-        "This is early days for gen 9. Lists will change as more information becomes available"
-    );
-    println!("Some names for gen 9 Pokemon, may be incorrect due to inaccurate translation or incorrect name");
-    println!(
-        "Feel free to file an issue at {} if something happens to be incorrect",
-        constants::REPO_ISSUES
-    );
-}
-
 pub mod string {
-    use super::generation_nine_early_days_warning;
     use crate::generation::Generation;
 
     pub trait StringExtension {
@@ -30,10 +16,7 @@ pub mod string {
                 "Kalos" => &Generation::Kalos,
                 "Alola" => &Generation::Alola,
                 "Galar" => &Generation::Galar,
-                "Paldea" => {
-                    generation_nine_early_days_warning();
-                    return &Generation::Paldea;
-                }
+                "Paldea" => &Generation::Paldea,
                 _ => panic!(""),
             }
         }
